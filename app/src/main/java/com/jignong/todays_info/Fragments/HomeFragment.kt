87c8e443674a29fa3b1c55b2cc136c46a85ef331
jinho <let0000>
2021-11-16 16:08:26 +0900

    코로나 확진자 기능 함수 추가

--- app/src/main/java/com/jignong/todays_info/Fragments/HomeFragment.kt
+++ app/src/main/java/com/jignong/todays_info/Fragments/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.jignong.todays_info.Fragments
 
 import android.R
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,8 +10,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.SpinnerAdapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.jignong.todays_info.MainActivity
 import com.jignong.todays_info.databinding.FragmentHomeBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -23,11 +27,16 @@ class HomeFragment : Fragment() {
         private var mBinding : FragmentHomeBinding? = null
     }
 
+    lateinit var binding: FragmentHomeBinding
+
     private val weatherUrl = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query="
     private val covid19Url = "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EC%BD%94%EB%A1%9C%EB%82%98+%ED%99%95%EC%A7%84%EC%9E%90&oquery=%EC%A7%80%EC%97%AD+%EB%82%A0%EC%94%A8&tqi=hS%2FEPdprvhGss5t4i6Nssssss6C-509210"
     private val citycovidUrl = "http://ncov.mohw.go.kr/"
 
     private val cityarray = arrayOfNulls<String>(18)
+    var weather_city = arrayOf("서울", "부산", "대구", "인천", "광주", "대전", "울산", "세종", "경기", "강원","충북","충남", "전북", "전남", "경북", "경남", "제주")
+
+    lateinit var mainActivity : MainActivity
 
     lateinit var weather_textview: TextView
     lateinit var totalcovid_textview: TextView
@@ -40,14 +49,15 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
 
         weather_textview = binding.weatherTextview
         totalcovid_textview = binding.totalcoivdTextview
         covid_textview = binding.coivdTextview
 
         //날씨 스피너
-        var weather_city = resources.getStringArray(R.array.weather_city)
+        //var weather_city = resources.getStringArray(R.array.weather_city)
+        //var weather_adapter = ArrayAdapter<String>(requireContext(), R.layout.simple_list_item_1, weather_city)
         var weather_adapter = ArrayAdapter<String>(requireContext(), R.layout.simple_list_item_1, weather_city)
         binding.weatherSpinner.adapter = weather_adapter
         binding.weatherSpinner.setSelection(0)
@@ -118,6 +128,8 @@ class HomeFragment : Fragment() {
             }
         }
 
+//        getCovid(covid19Url, totalcovid_textview)
+
         mBinding = binding
 
         return mBinding?.root
@@ -135,11 +147,28 @@ class HomeFragment : Fragment() {
             val tem = temple.get(0).text().substring(5)
 
             CoroutineScope(Dispatchers.Main).launch {
-                textView.text = "$city 날씨는 : $tem"
+                textView.text = "$city 의 날씨는 : $tem"
                 //Log.d(TAG, "select : $temple")
                 Log.d(TAG, "$city : $tem")
             }
         }
     }
+
+//    private fun getCovid(Url : String , textView: TextView ){
+//        CoroutineScope(Dispatchers.IO).launch {
+//            val doc = Jsoup.connect(Url).get()
+//            val total = doc.select("#content > div.container > div > div.liveboard_layout > div.liveNumOuter > div.liveNum > ul > li:nth-child(1) > span.num")
+//            val totalcovid = total.text().substring(0)
+//            Log.d(TAG, "확진자: $totalcovid")
+//            val today = doc.select("#content > div.container > div > div.liveboard_layout > div.liveNumOuter > div.liveNum > ul > li:nth-child(1) > span.before")
+//            val todaycovid = today.text().split(" ",")")
+//
+//            CoroutineScope(Dispatchers.Main).launch {
+//                textView.text = "총 확진자 :$totalcovid\n오늘의 확진자 :${todaycovid[2]}"
+//                Log.d(TAG, "총 확진자: $totalcovid")
+//                Log.d(TAG, "오늘의 확진자: ${todaycovid[2]}")
+//            }
+//        }
+//    }
 
 }
